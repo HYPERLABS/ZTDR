8e0ef54d31590b83f8b0d8eac74e0f7a8b7e83a8
BrianDoxey <bdoxey@gmail.com>
2014-09-18 08:41:16 -0500

    Version # sync

--- CVI/ZTDR_1XX.h
+++ CVI/ZTDR_1XX.h
@@ -1,7 +1,7 @@
 //==============================================================================
 //
 // Title:		ZTDR_1XX.h
-// Release:		1.0.2 (09/16/2014)
+// Release:		1.0.3 (09/18/2014)
 // Purpose:		ZTDR driver module and DLL functionality (v1.x.x)
 //
 // Copyright:	(c) 2014, HYPERLABS INC. All rights reserved.
