8dd2e983e1bfca53e461ebed2f8d4b937ad518c2
Brian Doxey <bdoxey@gmail.com>
2016-06-28 20:37:09 -0400

    Revert "Attempt at setParam fix"
    This reverts commit 35ff408fba14b1759216751eb4495d215bb95c76.

--- CVI/ZTDR_2XX.c
+++ CVI/ZTDR_2XX.c
@@ -1233,23 +1233,10 @@ __stdcall int usbfifo_setparams (UINT8 freerun_en, UINT16 calstart, UINT16 calen
 	params[IDX_STROBECNT_UPPER] = strobecount >> 8;
 	params[IDX_STROBECNT_LOWER] = (UINT8) strobecount;
 
-	
-	FT_STATUS status;
-	char open = 'o';
-	char close = 'c';
-	
-	// Open flow control
-	status = FT_Write (serialHandle, &open, 1, &n);
-	
+	ftwrbyte('p');
 	ret = FT_Write (serialHandle, params, NPARAMS, &n);
-	
-	status = FT_Write (serialHandle, &close, 1, &n);
-	
-	
-	
 	ch = ftrdbyte();
 
-	/*
 	if (ch != '.')
 	{
 		// No record received
@@ -1261,7 +1248,6 @@ __stdcall int usbfifo_setparams (UINT8 freerun_en, UINT16 calstart, UINT16 calen
 		// Incorrect number of params passed
 		return -2;
 	}
-	*/
 
 	return 1;
 }
